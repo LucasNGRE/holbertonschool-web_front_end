b1c7cb2839bcffe03329125909382c9f8dbae006
LucasNGRE <lucas12negre@gmail.com>
2025-05-16 10:35:57 +0200

    1. Create cookie with expiration date and specific path

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,22 +1,25 @@
 function setCookies() {
-    const firstname = document.getElementById("firstname").value;
-    const email = document.getElementById("email").value;
+  const firstname = document.getElementById("firstname").value;
+  const email = document.getElementById("email").value;
 
-    document.cookie = `firstname=${firstname}`;
-    document.cookie = `email=${email}`;
-  }
-  
-  function showCookies() {
-    const cookies = document.cookie.split("; ");
-    const myCookies = cookies.filter(cookie => 
-      cookie.startsWith("firstname=") || cookie.startsWith("email=")
-    );
-  
-    const p = document.createElement("p");
-    p.innerHTML = `Cookies: ${myCookies.join("; ")}`;
-    document.body.appendChild(p);
-  }
-  
-  window.setCookies = setCookies;
-  window.showCookies = showCookies;
-  
+  const date = new Date();
+  date.setTime(date.getTime() + (10 * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+
+  document.cookie = `firstname=${firstname}; ${expires}; path=/`;
+  document.cookie = `email=${email}; ${expires}; path=/`;
+}
+
+function showCookies() {
+  const cookies = document.cookie.split("; ");
+  const myCookies = cookies.filter(cookie =>
+    cookie.startsWith("firstname=") || cookie.startsWith("email=")
+  );
+
+  const p = document.createElement("p");
+  p.innerHTML = `Cookies: ${myCookies.join("; ")}`;
+  document.body.appendChild(p);
+}
+
+window.setCookies = setCookies;
+window.showCookies = showCookies;
